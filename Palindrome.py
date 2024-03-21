3a35c03066975ea5b2012731c9cdffd9e0eb7ac9
Michael Kemp <michaelandrewkemp99@gmail.com>
2024-03-22 01:24:58 +1300

    Create Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,10 @@
+# O(n) complexity and space is O(1)
+def isPalindrome(string):
+    # Write your code here.
+    right = -1
+    for i in range(len(string)):
+        if(string[i] != string[right]):
+            return False
+        else:
+            right = right - 1
+    return True
