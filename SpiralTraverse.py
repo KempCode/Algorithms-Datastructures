f4911e5f91f2512baa5b2fada08f176a572f76be
Michael Kemp <michaelandrewkemp99@gmail.com>
2024-04-02 14:24:27 +1300

    Update SpiralTraverse.py

--- SpiralTraverse.py
+++ SpiralTraverse.py
@@ -1,46 +1,49 @@
 def spiralTraverse(array):
     # Write your code here.
-    print("Vertical length", len(array))
-    print("Horizontal length", len(array[0]))
+    print("Vertical length",len(array))
+    print("Horizontal length",len(array[0]))
 
-    if len(array) == 1 and len(array[0]) == 1:
+    if(len(array) == 1 and len(array[0]) == 1):
         return array[0]
 
     start_row = 0
     end_row = len(array) - 1
-    start_col = 0
+    start_col = 0 
     end_col = len(array[0]) - 1
 
     output_array = []
-
-    while start_row <= end_row and start_col <= end_col:
+    
+    while(start_row <= end_row and start_col <= end_col):
 
         # Looping through N, E, S, W perimeters. 4 loops.
-
-        # North
         for i in range(start_col, end_col + 1):
             output_array.append(array[start_row][i])
-
         start_row += 1
 
+        # this is for arrays with one row:
+        if(start_row > end_row):
+            break
+    
         # East
         for i in range(start_row, end_row + 1):
             output_array.append(array[i][end_col])
-
         end_col -= 1
 
-        # South
-        if start_row <= end_row:
-            for i in range(end_col, start_col - 1, -1):
-                output_array.append(array[end_row][i])
-
-            end_row -= 1
-
+        # arrays with one column
+        if(start_col > end_col):
+            break
+                
+        # South 
+        for i in reversed(range(start_col, end_col + 1)):
+            output_array.append(array[end_row][i])
+        end_row -= 1
+        
         # West
-        if start_col <= end_col:
-            for i in range(end_row, start_row - 1, -1):
-                output_array.append(array[i][start_col])
-
-            start_col += 1
-
+        for i in reversed(range(start_row, end_row + 1)):
+            output_array.append((array[i][start_col]))
+        start_col += 1
+       
+        
     return output_array
+ 
+    
