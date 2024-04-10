fbeab263615f9336c50fa0cefa0daab90e3975cd
Michael Kemp <michaelandrewkemp99@gmail.com>
2024-04-10 21:13:32 +1200

    Update ArrayOfProducts.py
    Added the optimal complexity solution  for array of products question

--- ArrayOfProducts.py
+++ ArrayOfProducts.py
@@ -20,5 +20,31 @@ def arrayOfProducts(array):
             
 
 # Second the optimal complexity solution
+# time complexity is O(3N) space complexity is O(N)
+def arrayOfProducts(array):
+    # at pos i in left array, the value is all product of all values to left of pos i in original array
+    left_array = [1 for _ in range(len(array))]
+    # at pos i in right array, the value is all product of all values to right of pos i in original array
+    right_array = [1 for _ in range(len(array))]
+    #output array
+    products = [1 for _ in range(len(array))]
+
+    # fill left array
+    left_product = 1
+    for i in range(len(array)):
+        left_array[i] = left_product
+        left_product *= array[i]
+
+    # fill right array
+    right_product = 1
+    for i in reversed(range(len(array))):
+        right_array[i] = right_product
+        right_product *= array[i]
+
+    for i in range(len(array)):
+        products[i] = left_array[i] * right_array[i]
+
+    return products
+
 
 
