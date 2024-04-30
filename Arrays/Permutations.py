3390952797a6f4754a2914341a0c6f00602d3340
Michael Kemp <michaelandrewkemp99@gmail.com>
2024-04-30 21:00:27 +1200

    Create Permutations.py

---
+++ Arrays/Permutations.py
@@ -0,0 +1,17 @@
+def getPermutations(array):
+    # Write your code here.
+    output = []
+    permutations_helper(array, 0, output)
+    return output
+
+def permutations_helper(array, i, output):
+    if i == len(array) - 1:
+        output.append(array[:])
+    else:
+        for j in range(i, len(array)):
+            array[i], array[j] = array[j], array[i]
+            permutations_helper(array, i + 1, output)
+            array[i], array[j] = array[j], array[i]  # backtrack
+
+# Test the function
+print(getPermutations([1, 2, 3]))
