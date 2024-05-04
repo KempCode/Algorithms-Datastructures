eb83488a6e328e69424ccba50f6c7da8cc36ae6b
Michael Kemp <michaelandrewkemp99@gmail.com>
2024-05-04 22:59:20 +1200

    Update Permutations.py
    Added another solution

--- Arrays/Permutations.py
+++ Arrays/Permutations.py
@@ -16,3 +16,19 @@ def permutations_helper(array, i, output):
 # Function test
 print(getPermutations([1, 2, 3]))
 print(getPermutations([1, 5, 6, 7, 9]))
+
+
+
+def getPermutations(array):
+    def backtrack(start, end):
+        if start == end:
+            output.append(array[:])  # Append a copy of the current permutation
+        else:
+            for i in range(start, end + 1):
+                array[start], array[i] = array[i], array[start]  # Swap elements
+                backtrack(start + 1, end)  # Recur to the next position
+                array[start], array[i] = array[i], array[start]  # Backtrack
+
+    output = []
+    backtrack(0, len(array) - 1)
+    return output
