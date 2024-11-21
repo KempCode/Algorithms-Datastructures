0edb3e602c5685557e31f2e3fa49c01216bb5a34
Michael Kemp <15764472+KempCode@users.noreply.github.com>
2024-11-22 09:44:28 +1300

    Update FirstNonRepeatingChar.py
    Added Optimal Solution

--- Strings/FirstNonRepeatingChar.py
+++ Strings/FirstNonRepeatingChar.py
@@ -15,3 +15,23 @@ def firstNonRepeatingCharacter(string):
             return current_char
         
     return -1
+
+
+#O(N) time O(1) space best case O(N) worst Case.
+
+def firstNonRepeatingCharacter(string):
+    my_dict = {}
+    for i in range(len(string)):
+        if string[i] in my_dict:
+            # mydict{"a":(position, character frequency)}
+            my_dict[string[i]] = (my_dict[string[i]][0], my_dict[string[i]][1] + 1)
+        else:
+            my_dict[string[i]] = (i,1)
+
+    # They chars are added in order they are found in original array, but we still need original index hence the 
+    # Tuple
+    for i in my_dict:
+        if my_dict[i][1] == 1:  
+            return my_dict[i][0] #return stored position of char from original array
+     
+    return -1
